61e490e907e3155669388c8a7c5b834470c7d114
LinLai <zzbsn.cn@outlook.com>
2026-04-08 21:00:40 +0800

    1

---
+++ 数据结构/主文档/02.线性表/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+} Node,linklist ;
+
+int main()
+{
+    int n,i,e;
+    scanf("%d%d%d",&n,&i,&e);
+    Node *L[n];
+    
+    return 0;
+}
+void insert_data(Node *L)
+{
+
+}
